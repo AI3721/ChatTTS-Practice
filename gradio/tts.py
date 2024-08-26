d04a3e4ff2c9b8b012190b31b59a3c6bc167feec
AI3721 <111349596+AI3721@users.noreply.github.com>
2024-08-26 14:09:41 +0800

    Update tts.py

--- gradio/tts.py
+++ gradio/tts.py
@@ -1,10 +1,13 @@
 import ex
+import torch
 import random
 import requests
+import torchaudio
 import numpy as np
 import gradio as gr
 
 api = "http://127.0.0.1:7788"
+INT16_MAX = np.iinfo(np.int16).max
 has_interrupted = False
 seed_max = 99999999
 seed_min = 1
@@ -102,6 +105,36 @@ def generate_audio(text, audio_seed, stream, temperature, top_P, top_K, spk_emb,
                 list = add + list + add
             return list
         yield [np.array(drop_fill(wav), dtype=np.int16) for wav in response.json()]
+# 增强音频
+def enhance_audio(audio, audio_denoise, audio_enhance, CFM_method, CFM_temperature, CFM_strength, CFM_eval):
+    from enhance.enhancer.inference import denoise, enhance
+    if isinstance(audio, str):
+        dwav, sr = torchaudio.load(audio) # 双声道, float32
+        dwav = dwav.mean(dim=0) # 转为单声道
+        if audio_denoise:
+            new_dwav, new_sr = denoise(dwav, sr, device='cuda')
+        if audio_enhance:
+            new_dwav, new_sr = enhance(dwav, sr, device='cuda', solver=CFM_method.lower(),
+                                       nfe=CFM_eval, lambd=CFM_strength, tau=CFM_temperature)
+        new_dwav = new_dwav.unsqueeze(0).cpu() # 转为双声道
+        new_audio = "/tmp/gradio/enhanced_audio.wav" #
+        torchaudio.save(new_audio, new_dwav, new_sr)
+        torch.cuda.empty_cache()
+        yield new_audio
+    if isinstance(audio, tuple):
+        sr, dwav = audio # int16, 单声道
+        dwav = dwav.astype(np.float32)/INT16_MAX # 转为float32
+        dwav = torch.tensor(dwav,dtype=torch.float32)
+        if audio_denoise:
+            new_dwav, new_sr = denoise(dwav, sr, device='cuda')
+        if audio_enhance:
+            new_dwav, new_sr = enhance(dwav, sr, device='cuda', solver=CFM_method.lower(),
+                                       nfe=CFM_eval, lambd=CFM_strength, tau=CFM_temperature)
+        new_dwav = new_dwav.cpu().numpy() # 转移到cpu内存，再转换为numpy数组
+        new_dwav = (new_dwav*INT16_MAX).astype(np.int16) # 转为int16
+        new_audio = new_sr, new_dwav
+        torch.cuda.empty_cache()
+        yield new_audio
 
 
 # 更换speaker
@@ -140,33 +173,55 @@ def create_tts_block():
             with gr.Row():
                 with gr.Column(min_width=0, scale=2):
                     with gr.Tab(label="Input Text"):
-                        input_text = gr.Textbox(label="", lines=9, max_lines=9, show_copy_button=True, interactive=True, value=ex.input_text, elem_id="input_text")
+                        input_text = gr.Textbox(label="", lines=12, max_lines=12, show_copy_button=True, interactive=True, value=ex.input_text, elem_id="input_text")
                     with gr.Tab(label="Sample Text"):
-                        sample_text = gr.Textbox(label="", lines=9, max_lines=9, show_copy_button=True, interactive=True)
-                    with gr.Row(): # 文本插入辅助控制词
-                        laugh_btn = gr.Button("笑声 + [laugh]", min_width=0)
-                        laugh_btn.click(fn=None, outputs=input_text, js=js_insert("[laugh]"))
-                        lbreak_btn = gr.Button("长停顿 + [lbreak]", min_width=0)
-                        lbreak_btn.click(fn=None, outputs=input_text, js=js_insert("[lbreak]"))
-                        uv_break_btn = gr.Button("短停顿 + [uv_break]", min_width=0)
-                        uv_break_btn.click(fn=None, outputs=input_text, js=js_insert("[uv_break]"))
+                        sample_text = gr.Textbox(label="", lines=12, max_lines=12, show_copy_button=True, interactive=True, value="空")
                 with gr.Column(min_width=0, scale=1):
                     with gr.Tab(label="Sample Audio"):
                         sample_audio = gr.Audio(label="", type="filepath", waveform_options=gr.WaveformOptions(sample_rate=24000))
                     with gr.Tab(label="Emb"):
-                        sample_emb = gr.Textbox(label="", lines=11, max_lines=11, show_copy_button=True, interactive=True)
+                        sample_emb = gr.Textbox(label="", lines=12, max_lines=12, show_copy_button=True, interactive=True)
+            # Button
+            with gr.Row():
+                with gr.Column(min_width=0, scale=2):
+                    with gr.Row(): # 文本插入辅助控制词
+                        laugh_btn = gr.Button("笑声 + [laugh]", min_width=0)
+                        lbreak_btn = gr.Button("长停顿 + [lbreak]", min_width=0)
+                        uv_break_btn = gr.Button("短停顿 + [uv_break]", min_width=0)
+                with gr.Column(min_width=0, scale=1):
+                    with gr.Row(): # 音频降噪或音频增强
+                        is_ture = gr.Checkbox(value=True, visible=False)
+                        is_false = gr.Checkbox(value=False, visible=False)
+                        denoise_btn = gr.Button("音频降噪", min_width=0, interactive=True)
+                        enhance_btn = gr.Button("音频增强", min_width=0, interactive=True)
             # 可调参数
             with gr.Accordion(label="可调参数：点击这里，可以尝试生成不同音色的声音！", open=False):
                 # Checkbox
                 with gr.Row():
-                    auto_play = gr.Checkbox(label="Auto Play", value=False, interactive=True)
-                    stream_mode = gr.Checkbox(label="Stream Mode", value=False, interactive=True)
-                    text_refine = gr.Checkbox(label="Text Refine", value=True, interactive=True)
+                    with gr.Column(min_width=0):
+                        CFM_method = gr.Dropdown(label="CFM_Method", choices=["Midpoint", "Euler", "RK4"], value="Midpoint", interactive=True, container=False)
+                    with gr.Column(min_width=0):
+                        audio_denoise = gr.Checkbox(label="Audio Denoise", value=False, interactive=True)
+                    with gr.Column(min_width=0):
+                        audio_enhance = gr.Checkbox(label="Audio Enhance", value=True, interactive=True)
                 # Slider
                 with gr.Row():
-                    temperature = gr.Slider(label="Temperature", minimum=0, maximum=1.0, step=0.01, value=0.3, interactive=True)
-                    top_p = gr.Slider(label="top_P", minimum=0, maximum=1.0, step=0.05, value=0.7, interactive=True)
-                    top_k = gr.Slider(label="top_K", minimum=0, maximum=20, step=1, value=10, interactive=True)
+                    CFM_temperature = gr.Slider(label="CFM_Temperature", minimum=0, maximum=1, step=0.01, value=0.3, interactive=True, min_width=0)
+                    CFM_strength = gr.Slider(label="CFM_Strength", minimum=0, maximum=1, step=0.05, value=0.7, interactive=True, min_width=0)
+                    CFM_eval = gr.Slider(label="CFM_Eval", minimum=1, maximum=128, step=1, value=64, interactive=True, min_width=0)
+                # Checkbox
+                with gr.Row():
+                    with gr.Column(min_width=0):
+                        auto_play = gr.Checkbox(label="Auto Play", value=False, interactive=True, min_width=0)
+                    with gr.Column(min_width=0):
+                        stream_mode = gr.Checkbox(label="Stream Mode", value=False, interactive=True, min_width=0)
+                    with gr.Column(min_width=0):
+                        text_refine = gr.Checkbox(label="Text Refine", value=True, interactive=True, min_width=0)
+                # Slider
+                with gr.Row():
+                    temperature = gr.Slider(label="Temperature", minimum=0, maximum=1, step=0.01, value=0.3, interactive=True, min_width=0)
+                    top_p = gr.Slider(label="top_P", minimum=0, maximum=1, step=0.05, value=0.7, interactive=True, min_width=0)
+                    top_k = gr.Slider(label="top_K", minimum=1, maximum=20, step=1, value=10, interactive=True, min_width=0)
                 # Tab
                 with gr.Row():
                     with gr.Column(min_width=0):
@@ -189,7 +244,15 @@ def create_tts_block():
             # Button
             generate_btn = gr.Button("Generate", variant="primary")
             interrupt_btn = gr.Button("Interrupt", variant="stop", visible=False)
-            output_text = gr.Textbox(label="Output Text", lines=4, max_lines=4, show_copy_button=True)
+            output_text = gr.Textbox(label="Output Text", lines=4, max_lines=4, show_copy_button=True, interactive=False)
+            # 响应事件
+            laugh_btn.click(fn=None, outputs=input_text, js=js_insert("[laugh]"))
+            lbreak_btn.click(fn=None, outputs=input_text, js=js_insert("[lbreak]"))
+            uv_break_btn.click(fn=None, outputs=input_text, js=js_insert("[uv_break]"))
+            audio_denoise.change(fn=lambda a,b: (a!=True)&b, inputs=[audio_denoise, audio_enhance], outputs=audio_enhance, show_progress=False)
+            audio_enhance.change(fn=lambda a,b: (a!=True)&b, inputs=[audio_enhance, audio_denoise], outputs=audio_denoise, show_progress=False)
+            denoise_btn.click(fn=enhance_audio, inputs=[sample_audio, is_ture, is_false, CFM_method, CFM_temperature, CFM_strength, CFM_eval], outputs=sample_audio)
+            enhance_btn.click(fn=enhance_audio, inputs=[sample_audio, is_false, is_ture, CFM_method, CFM_temperature, CFM_strength, CFM_eval], outputs=sample_audio)
             # 响应事件
             sample_audio.change(fn=change_sample, inputs=sample_audio, outputs=sample_emb)
             speaker_btn.click(fn=random_speaker, outputs=speaker)
@@ -207,6 +270,11 @@ def create_tts_block():
                     label="Output Audio", interactive=False,
                     format='mp3' if not streaming else 'wav',
                     waveform_options=gr.WaveformOptions(sample_rate=24000))
+                enhanced_audio = gr.Audio(
+                    autoplay=autoplay, streaming=streaming,
+                    label="Enhanced Audio", interactive=False,
+                    format='mp3' if not streaming else 'wav',
+                    waveform_options=gr.WaveformOptions(sample_rate=44100))
                 generate_btn.click(
                     fn=set_button_before, 
                     inputs=[generate_btn, interrupt_btn],
@@ -220,10 +288,15 @@ def create_tts_block():
                     inputs=[output_text, audio_seed, stream_mode, temperature, top_p, top_k, spk_emb, sample_emb, sample_text],
                     outputs=output_audio
                 ).then(
+                    fn=enhance_audio,
+                    inputs=[output_audio, audio_denoise, audio_enhance, CFM_method, CFM_temperature, CFM_strength, CFM_eval],
+                    outputs=enhanced_audio
+                ).then(
                     fn=set_button_after,
                     inputs=[generate_btn, interrupt_btn],
                     outputs=[generate_btn, interrupt_btn]
                 )
+            
             # 更多玩法
             with gr.Accordion(label="更多玩法：点击这里，可以体验更多新奇有趣的玩法！", open=False):
                 from tts_more import create_tts_more_block
